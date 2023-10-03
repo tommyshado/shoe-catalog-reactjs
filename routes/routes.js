0abfbcb6de486d27dccb0543bf8ab6d3d568bfb2
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-03 22:57:24 +0200

    get the data with axios, rendered the shoes from the database to the front end

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import axios from "axios";
 
 // Router instance
 const router = Router();
 
 
-router.get("/", (req, res) => {
-    res.render("index");
+router.get("/", async (req, res) => {
+    // GET the shoes from the api
+    const shoes = (await axios.get("https://api-for-shoes.onrender.com/api/shoes")).data;
+    // RENDER the shoes in the index page
+    res.render("index", {
+        shoe: shoes,
+    });
 });
 
 export default router;
