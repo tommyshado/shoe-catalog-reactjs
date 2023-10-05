4f6f1a564d0fa65aec4f72a0e702d876d98db2b6
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-05 15:22:18 +0200

    rendered the addShoe page

--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,35 @@ import "dotenv/config";
 // Router instance
 const router = Router();
 
+// Api endpoint
+const API_END_POINT = process.env.API_END_POINT;
 
+// Router to get the shoes from the database and display the data to the ui
 router.get("/", async (req, res) => {
     // GET the shoes from the api
-    const shoes = (await axios.get(`${process.env.API_KEY}`)).data;
+    const shoes = (await axios.get(`${API_END_POINT}`)).data;
     // RENDER the shoes in the index page
     res.render("index", {
         shoe: shoes,
     });
+});
+
+router.get("/addShoes", (req, res) => {
+    res.render("addShoe");
+});
+
+// Router to insert a shoe into the ui and database
+router.post("/addShoes", async (req, res) => {
+    const { shoeName, image, qty, shoePrice, shoeColor, shoeSize } = req.body;
+    const responsed = await axios.post(`${API_END_POINT}`, {
+        shoeName,
+        image,
+        qty,
+        shoePrice,
+        shoeColor,
+        shoeSize,
+    });
+    res.send(responsed.data);
 });
 
 export default router;
