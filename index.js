3fc794c38c08434b2d0685e218ab845d28a7fd74
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-05 15:23:05 +0200

    imported body-parser and used the bodyParser middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import exphbs from "express-handlebars";
 import router from "./routes/routes.js";
+import bodyParser from "body-parser";
 
 // APP instance
 const app = express();
+
 
 // Handlebars view engine
 const handlebarSetup = exphbs.engine({
@@ -16,12 +18,17 @@ app.engine("handlebars", handlebarSetup);
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 // Routes Middlewares
-app.use("/", router);
+app.use("/api/shoes", router);
 
 // PORT variable
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("🚀 app started at PORT", PORT);
+  console.log("🚀 app started at PORT", PORT);
 });
