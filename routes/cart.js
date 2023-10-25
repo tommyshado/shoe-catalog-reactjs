5a11f46a8d4f8205d5e1195fbad4da4d991e8b48
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-25 09:43:15 +0200

    removed the header cnfiguration for now

--- routes/cart.js
+++ routes/cart.js
@@ -7,18 +7,10 @@ const cartAPI = "https://api-for-shoes.onrender.com/api/cart";
 // Router instance
 const cartRouter = Router();
 
-// Header
-const headers = {
-    'Content-Type': 'application/json',
-    'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlbmRhbmlAZ21haWwuY29tIiwiaWF0IjoxNjk4MTY3NTMxfQ.EuI53PxGOBOhonRzkQ7aA4qys7X0yLT9zRzHXgg39g8'
-};
-
 // Router to get the shoes added to the shopping cart
 cartRouter.get("/username/:username", async (req, res) => {
     const username = req.params.username;
-    const data = (await axios.get(cartAPI + `/username/${username}`, {
-        headers: headers
-    })).data;
+    const data = (await axios.get(cartAPI + `/username/${username}`)).data;
 
     // GET cart and total
     const cart = data.cart;
